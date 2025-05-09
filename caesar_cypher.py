1d60864427d20f1ea24a28f89a8e03c6614c8bee
jain-d <jain.divyansh.0017@gmail.com>
2025-05-10 02:57:20 +0530

    caesar cypher

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,7 +1,20 @@
 # caesar cypher is a technique of cyphering in which we replace the letter with its third consecutive letter in the alphabets. 
+def encode(message, shift=3):
+    cyphered = ""
+    for letter in message:
+        ascii_value = ord(letter) + shift
+        if ascii_value > 90 and ascii_value < 97:
+            cyphered += chr((ascii_value - 91) + ord("A"))
+        elif ascii_value > 120:
+            cyphered += chr((ascii_value - 123) + ord("a"))
+        else:
+            cyphered += chr(ascii_value)
+    return cyphered
+        
+
 
 def main():
-    alphabets = 'abcdefghijklmnopqrstuvwxyz'
     message = input("What is to be cyphered? ")
-        
+    print(f"cypher - {encode(message)}")
+
 main()
