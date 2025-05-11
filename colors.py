4adfe9e301b3c853f232dbbc78efc835e5e9d006
jain-d <jain.divyansh.0017@gmail.com>
2025-05-11 19:02:49 +0530

    ANSI color codes

---
+++ colors.py
@@ -0,0 +1,11 @@
+class Colors:
+    RED = '\033[31m'
+    PINK = '\033[38;5;213m'
+    GREEN = '\033[38;5;2m'
+    YELLOW = '\033[38;5;11m'
+    BLUE = '\033[38;5;33m'
+    ORANGE = '\033[38;5;208m'
+    GREY = '\033[38;5;245m'
+    RESET = '\033[0m'
+
+colors = Colors();
